71937a49b016700d7ad94e40956585e6aba569d5
jk0011 <jk0011@jk0011.com>
2019-04-20 20:20:20 +0800

    v1.0.0

---
+++ src/Operator/Add/Add.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaoyu
+ * Date: 19-4-20
+ * Time: 下午3:54
+ */
+namespace Operator\Add;
+
+class Add
+{
+    public function add($n1, $n2) {
+        return $n1 + $n2;
+    }
+}
---
+++ src/Operator/Sub/Sub.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaoyu
+ * Date: 19-4-20
+ * Time: 下午3:55
+ */
+
+namespace Operator\Sub;
+
+class Sub
+{
+    public function sub($n1, $n2) {
+        return $n1 - $n2;
+    }
+}
